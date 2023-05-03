b536b5d1ed5474b8ce4db002bfd015b5ed9b228b
thuy <109534863+Thuy2y2c@users.noreply.github.com>
2023-05-03 14:28:25 +0700

    ui

--- tools/uitest.lua
+++ tools/uitest.lua
@@ -4,20 +4,16 @@ for i,v in next, game.Players.LocalPlayer.PlayerGui:GetChildren() do -- Deletes 
     end
 end
 
----- // GUI // ----
-
 -- Instances:
 
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local Name = Instance.new("TextLabel")
 local Username = Instance.new("TextLabel")
-local JobID = Instance.new("TextLabel")
-local Line = Instance.new("Frame")
-local Koishi = Instance.new("ImageLabel")
-local Mirage = Instance.new("TextLabel")
-local FullMoon = Instance.new("TextLabel")
+local JobId = Instance.new("TextLabel")
+local credits = Instance.new("TextLabel")
+local Tip = Instance.new("TextLabel")
+local cat = Instance.new("ImageLabel")
 
 --Properties:
 
@@ -27,73 +23,77 @@ blur.Size = 100
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(255, 240, 249)
-Frame.Position = UDim2.new(0.5, -325, 0.5, -150)
-Frame.Size = UDim2.new(0, 650, 0, 300)
+Frame.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+Frame.Position = UDim2.new(0.5, -400, 0.5, -200)
+Frame.Size = UDim2.new(0, 800, 0, 400)
 
 UICorner.Parent = Frame
-
-Name.Name = "Name"
-Name.Parent = Frame
-Name.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Name.BackgroundTransparency = 1.000
-Name.BorderSizePixel = 0
-Name.Position = UDim2.new(0.224999994, 0, 0.0500000007, 0)
-Name.Size = UDim2.new(0, 145, 0, 35)
-Name.Font = Enum.Font.ArialBold
-Name.Text = "catn1qqer"
-Name.TextColor3 = Color3.fromRGB(207, 132, 142)
-Name.TextScaled = true
-Name.TextSize = 19.000
-Name.TextWrapped = true
 
 Username.Name = "Username"
 Username.Parent = Frame
 Username.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Username.BackgroundTransparency = 1.000
 Username.BorderSizePixel = 0
-Username.Position = UDim2.new(0.0153846145, 0, 0.218333334, 0)
-Username.Size = UDim2.new(0, 620, 0, 30)
-Username.Font = Enum.Font.ArialBold
-Username.Text = "Username : <username>"
-Username.TextColor3 = Color3.fromRGB(207, 132, 142)
-Username.TextScaled = true
-Username.TextSize = 19.000
+Username.Position = UDim2.new(0.0217857361, 0, 0.022499999, 0)
+Username.Size = UDim2.new(0, 571, 0, 50)
+Username.Font = Enum.Font.Gotham
+Username.Text = "Welcome, <username>"
+Username.TextColor3 = Color3.fromRGB(255, 255, 255)
+Username.TextSize = 39.000
 Username.TextWrapped = true
 Username.TextXAlignment = Enum.TextXAlignment.Left
 
-Line.Name = "Line"
-Line.Parent = Frame
-Line.BackgroundColor3 = Color3.fromRGB(235, 174, 255)
-Line.BorderSizePixel = 0
-Line.Position = UDim2.new(0, 10, 0.180000007, 0)
-Line.Size = UDim2.new(0, 420, 0, 1)
+JobId.Name = "JobId"
+JobId.Parent = Frame
+JobId.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+JobId.BackgroundTransparency = 1.000
+JobId.BorderSizePixel = 0
+JobId.Position = UDim2.new(0.0217857361, 0, 0.125, 0)
+JobId.Size = UDim2.new(0, 758, 0, 18)
+JobId.Font = Enum.Font.Gotham
+JobId.Text = "JobId : <jobid>"
+JobId.TextColor3 = Color3.fromRGB(255, 255, 255)
+JobId.TextSize = 14.000
+JobId.TextWrapped = true
+JobId.TextXAlignment = Enum.TextXAlignment.Left
 
-Koishi.Name = "Koishi"
-Koishi.Parent = Frame
-Koishi.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Koishi.BackgroundTransparency = 1.000
-Koishi.Position = UDim2.new(0.541538477, 0, -0.379999995, 0)
-Koishi.Size = UDim2.new(0, 370, 0, 470)
-Koishi.Image = "rbxassetid://10830943750"
-Koishi.ScaleType = Enum.ScaleType.Fit
+credits.Name = "credits"
+credits.Parent = Frame
+credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+credits.BackgroundTransparency = 1.000
+credits.BorderSizePixel = 0
+credits.Position = UDim2.new(0.901785791, 0, 0.914999962, 0)
+credits.Size = UDim2.new(0, 66, 0, 26)
+credits.Font = Enum.Font.Gotham
+credits.Text = "catn1qqer"
+credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+credits.TextSize = 14.000
+credits.TextTransparency = 0.360
+credits.TextWrapped = true
+credits.TextXAlignment = Enum.TextXAlignment.Right
 
-JobID.Name = "JobID" -- a button.
-JobID.Parent = Frame
-JobID.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-JobID.BackgroundTransparency = 1.000
-JobID.BorderSizePixel = 0
-JobID.Position = UDim2.new(0.0149999997, 0, 0.314999998, 0)
-JobID.Size = UDim2.new(0, 589, 0, 15)
-JobID.Font = Enum.Font.ArialBold
-JobID.Text = "JobID : <jobid>"
-JobID.TextColor3 = Color3.fromRGB(207, 132, 142)
-JobID.TextScaled = true
-JobID.TextSize = 14.000
-JobID.TextWrapped = true
-JobID.TextXAlignment = Enum.TextXAlignment.Left
+Tip.Name = "Tip"
+Tip.Parent = Frame
+Tip.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Tip.BackgroundTransparency = 1.000
+Tip.BorderSizePixel = 0
+Tip.Position = UDim2.new(0.159285739, 0, 0.4375, 0)
+Tip.Size = UDim2.new(0, 571, 0, 50)
+Tip.Font = Enum.Font.Gotham
+Tip.Text = "tat ca la tai cong san"
+Tip.TextColor3 = Color3.fromRGB(255, 255, 255)
+Tip.TextSize = 39.000
+Tip.TextWrapped = true
 
----- // UI CODE // ----
+cat.Name = "cat!!"
+cat.Parent = Frame
+cat.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+cat.BackgroundTransparency = 1.000
+cat.Position = UDim2.new(0.0212499946, 0, 0.730000019, 0)
+cat.Size = UDim2.new(0, 100, 0, 100)
+cat.Image = "rbxassetid://12711711186"
 
-JobID.Text = "JobID : ".. game.JobId
+-- // Code \\ --
+
+JobId.Text = "JobId : ".. game.JobId
 Username.Text = "Username : ".. game.Players.LocalPlayer.Name
